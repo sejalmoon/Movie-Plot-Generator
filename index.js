5fcdf07881d3d9146130de97be711b3422af7876
sejalmoon <sejalmoon1@gmail.com>
2024-01-01 00:43:40 +0530

    index files updated

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
+const { JSDOM } = require('jsdom');
+const { document } = (new JSDOM('')).window;
+
 const setupTextarea = document.getElementById('setup-textarea') 
 const setupInputContainer = document.getElementById('setup-input-container')
 const movieBossText = document.getElementById('movie-boss-text')
 
 document.getElementById("send-btn").addEventListener("click", () => {
   if (setupTextarea.value) {
-    setupInputContainer.innerHTML = `<img src="images/loading.svg" class="loading" id="loading">`
+    setupInputContainer.innerHTML = `<img src="./images/loading.svg" class="loading" id="loading">`
     movieBossText.innerText = `Ok, just wait a second while my digital brain digests that...`
   }
 })
