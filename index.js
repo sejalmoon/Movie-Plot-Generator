93b5ed046e9f52a7608adf3c84eb5fbd39107306
sejalmoon <sejalmoon1@gmail.com>
2024-01-03 13:45:03 +0530

    index file updated

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
-const { JSDOM } = require('jsdom');
-const { document } = (new JSDOM('')).window;
+import fetch from 'node-fetch';
 
-const setupTextarea = document.getElementById('setup-textarea') 
-const setupInputContainer = document.getElementById('setup-input-container')
-const movieBossText = document.getElementById('movie-boss-text')
 
-document.getElementById("send-btn").addEventListener("click", () => {
-  if (setupTextarea.value) {
-    setupInputContainer.innerHTML = `<img src="./images/loading.svg" class="loading" id="loading">`
-    movieBossText.innerText = `Ok, just wait a second while my digital brain digests that...`
-  }
-})
+
+const url = "https://api.openai.com/v1/completions"
+
+
+fetch(url, {
+  method: 'POST',
+  headers: {
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${apiKey}`
+  },
+  body: JSON.stringify({
+      'model': 'gpt-3.5-turbo-instruct',
+      'prompt': 'What is the capital of Spain?'
+  })
+}).then(response => response.json()).then(data => console.log(data))
